a64920c96c0b393f99845c41d3f2ef76bd4bbd3d
Bradley <brad.driver33@gmail.com>
2018-11-28 15:31:50 -0500

    finished coding the server.js file

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
-const express = requiere("express");
+const express = require("express");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+
+// Open Server 
+var PORT = process.env.PORT || 3000;
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(process.cwd() + "/public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
+
+app.use("/", routes);
+
+app.listen(port);
